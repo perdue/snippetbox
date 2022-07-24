23317b0037d94f8a46d581575934e0ef0287e228
Brent Perdue <brent.perdue@luminsmart.com>
2022-07-24 15:03:30 -0400

    Lock create to POST

--- main.go
+++ main.go
@@ -27,6 +27,28 @@ func snippetView(w http.ResponseWriter, r *http.Request) {
 
 // Add a snippetCreate handler function.
 func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	// Use r.Method to check whether the request is using POST or not.
+	if r.Method != "POST" {
+		// Use the Header().Set() method to add an 'Allow: POST' header to the
+		// response header map. The first parameter is the header name, and
+		// the second parameter is the header value.
+		//		w.Header().Set("Allow", "POST")
+		w.Header().Set("Allow", http.MethodPost)
+
+		// If it's not, use the w.WriteHeader() method to send a 405 status
+		// code and the w.Write() method to write a "Method Not Allowed"
+		// response body. We then return from the function so that the
+		// subsequent code is not executed.
+		//		w.WriteHeader(405)
+		//		w.Write([]byte("Method Not Allowed"))
+
+		// Use the http.Error() function to send a 405 status code and "Method
+		// Not Allowed" string as the response body.
+		// http.Error(w, "Method Not Allowed", 405)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
@@ -35,8 +57,8 @@ func main() {
 	// register the home function as the handler for the "/" URL pattern.
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home) // This is a subtree path, acting like a catch-all
-	mux.HandleFunc("/snippetView", snippetView)
-	mux.HandleFunc("/snippetCreate", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server. We pass in
 	// two parameters: the TCP network address to listen on (in this case ":4000")
